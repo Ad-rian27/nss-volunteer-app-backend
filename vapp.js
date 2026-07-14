a8228591de96ea8453ef9eb2ccc31582e643dafe
Ad-rian27 <rian270627@gmail.com>
2026-07-14 19:27:27 +0530

    Added add-volunteer api

--- vapp.js
+++ vapp.js
@@ -6,6 +6,42 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb+srv://adrian:adrian123@cluster0.veegpvo.mongodb.net/vdb").then(
+    () => {
+        console.log("MongoDB connected")
+    }
+).catch(
+    (err) => (
+        console.log(err)
+))
+
+const Volunteer=mongoose.model("Volunteers", new mongoose.Schema(
+    {
+        volunteerId: String,
+        fullName: String,
+        email: String,
+        phone: String,
+        dob: String,
+        gender: String,
+        bloodGroup: String,
+        dept: String,
+        yos: String,
+        campName: String,
+        hoursComp: String,
+        address: String,
+        unitNo: String
+    }
+))
+
 app.get("/test", (req, res) => {
     res.send("hello")
+})
+
+app.post("/add-volunteer", async (req,res) => {
+    await Volunteer.create(req.body)
+    res.json({"status" : "success"})
+})
+
+app.listen(3000,() => {
+    console.log("Server started")
 })
